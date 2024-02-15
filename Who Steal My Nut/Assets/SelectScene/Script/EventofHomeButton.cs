0a97de081f4a2a9e3c179c631ffbcfd4006f5b2b
splendid-atom <1549444829@qq.com>
2024-02-16 00:04:10 +0800

    commit2\15

---
+++ Who Steal My Nut/Assets/SelectScene/Script/EventofHomeButton.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EventofHomeButton : MonoBehaviour
+{
+    private bool animeStatus = false;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Log("HomeButton Start!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // <Animator Off->Hover>
+        if (collision.name == "mouse" && animeStatus == false)
+        {
+            animator.SetBool("isHovering", true);
+            animeStatus = true;
+        }
+        // </Animator Off->Hover>
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        // <Open Alert>
+        if (collision.name == "mouse" && animeStatus == true)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                SceneManager.LoadScene("Menu(WelcomeScene)");
+                animator.SetBool("isHovering", false);
+                animeStatus = false;
+            }
+        }
+        // </Open Alert>
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // <Animator Hover->Off>
+        if (collision.name == "mouse" && animeStatus == true)
+        {
+            animator.SetBool("isHovering", false);
+            animeStatus = false;
+        }
+        // </Animator Hover->Off>
+    }
+}
